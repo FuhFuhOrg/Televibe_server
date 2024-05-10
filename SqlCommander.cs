36e5fbb9282aed6ad01f3bfafefcfa95cdd17f4e
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2024-05-10 23:58:49 +0300

    Update SqlCommander.cs

--- SqlCommander.cs
+++ SqlCommander.cs
@@ -492,78 +492,97 @@ namespace shooter_server
 
         // Вернуть сообщения, которые больше id_msg +
         private async Task GetMessages(string sqlCommand, int senderId, NpgsqlConnection dbConnection, Lobby lobby, WebSocket ws)
-        {        
-            try
+{
+    try
+    {
+        using (var cursor = dbConnection.CreateCommand())
+        {
+            // GetMessages requestId kSenderId senderId[kSenderId] kIdMsg idMsg[kIdMsg]
+            List<string> credentials = new List<string>(sqlCommand.Split(' '));
+
+            credentials.RemoveAt(0);
+
+            int requestId = int.Parse(credentials[0]);
+            int kSenderId = int.Parse(credentials[1]);
+            int kIdMsg = int.Parse(credentials[2 + kSenderId]);
+
+            List<int> senderIds = new List<int>();
+            List<int> messageIds = new List<int>();
+
+            for (int i = 0; i < kSenderId; i++)
             {
-                using (var cursor = dbConnection.CreateCommand())
+                senderIds.Add(int.Parse(credentials[2 + i]));
+            }
+
+            for (int i = 0; i < kIdMsg; i++)
+            {
+                messageIds.Add(int.Parse(credentials[2 + kSenderId + i]));
+            }
+
+            List<Message> messages = new List<Message>();
+
+            for (int i = 0; i < kSenderId; ++i)
+            {
+                // Все айдишники кроме последнего
+                for (int j = 0; j < kIdMsg - 1; ++j)
                 {
-                    // GetMessages requestId kSenderId senderId[kSenderId] kIdMsg idMsg[kIdMsg]
-                    List<string> credentials = new List<string>(sqlCommand.Split(' '));
-        
-                    credentials.RemoveAt(0);
-        
-                    int requestId = int.Parse(credentials[0]);
-                    int kSenderId = int.Parse(credentials[1]);
-                    int kIdMsg = int.Parse(credentials[2 + kSenderId]);
-        
-                    List<int> senderIds = new List<int>();
-                    List<int> messageIds = new List<int>();
-        
-                    for (int i = 0; i < kSenderId; i++)
+                    cursor.Parameters.AddWithValue("idSender", senderIds[i]);
+                    cursor.Parameters.AddWithValue("messageId", messageIds[j]);
+
+                    cursor.CommandText = $"SELECT * FROM messages WHERE id_sender = @idSender AND id_msg = @messageId ORDER BY id_msg ASC";
+
+                    using (var reader = await cursor.ExecuteReaderAsync())
                     {
-                        senderIds.Add(int.Parse(credentials[2 + i]));
-                    }
-        
-                    for (int i = 0; i < kIdMsg; i++)
-                    {
-                        messageIds.Add(int.Parse(credentials[2 + kSenderId + i]));
-                    }
-        
-                    List<Message> messages = new List<Message>();
-        
-                    int idSender = 0;
-                    long lastMessageId = messageIds[kIdMsg - 1];
-        
-                    for (int i = 0; i < kSenderId; i++)
-                    {
-                        idSender = senderIds[i];
-        
-                        cursor.Parameters.AddWithValue("idSender", idSender);
-                        cursor.Parameters.AddWithValue("messageId", lastMessageId);
-        
-                        string sql = $"SELECT * FROM messages WHERE id_sender = @idSender AND id_msg >= @messageId ORDER BY id_msg ASC";
-                        cursor.CommandText = sql;
-        
-                        using (var reader = await cursor.ExecuteReaderAsync())
+                        await reader.ReadAsync();
+
+                        Message message = new Message
                         {
-                            while (await reader.ReadAsync())
-                            {
-                                Message message = new Message
-                                {
-                                    id_sender = reader.GetInt32(0),
-                                    id_msg = reader.GetInt32(1),
-                                    time_msg = reader.GetDateTime(2),
-                                    msg = reader.GetFieldValue<byte[]>(3),
-                                };
-        
-                                messages.Add(message);
-                            }
-                        }
+                            id_sender = reader.GetInt32(0),
+                            id_msg = reader.GetInt32(1),
+                            time_msg = reader.GetDateTime(2),
+                            msg = reader.GetFieldValue<byte[]>(3),
+                        };
+
+                        messages.Add(message);
                     }
-        
-                    string result = "";
-                    foreach (var message in messages)
+                }
+
+                // Все айдишники после последнего, включая последнего
+                cursor.Parameters.AddWithValue("idSender", senderIds[i]);
+                cursor.Parameters.AddWithValue("messageId", messageIds[j]);
+
+                cursor.CommandText = $"SELECT * FROM messages WHERE id_sender = @idSender AND id_msg >= @messageId ORDER BY id_msg ASC";
+
+                using (var reader = await cursor.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
                     {
-                        result += message.GetString();
+                        Message message = new Message
+                        {
+                            id_sender = reader.GetInt32(0),
+                            id_msg = reader.GetInt32(1),
+                            time_msg = reader.GetDateTime(2),
+                            msg = reader.GetFieldValue<byte[]>(3),
+                        };
+
+                        messages.Add(message);
                     }
-        
-                    lobby.SendMessagePlayer($"/ans {result}", ws, requestId);
                 }
             }
-            catch (Exception e)
+
+            string result = "";
+            foreach (var message in messages)
             {
-                Console.WriteLine($"Error GetMessages command: {e}");
+                result += message.GetString();
             }
+
+            lobby.SendMessagePlayer($"/ans {result}", ws, requestId);
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error GetMessages command: {e}");
+    }
 }
 
 
