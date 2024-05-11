8456caad80bb2ac1d54777a84c5c9c52f612d007
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2024-05-11 08:26:55 +0300

    upd

--- obj/Debug/net8.0/shooter_server.AssemblyInfo.cs
+++ obj/Debug/net8.0/shooter_server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shooter_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d24f95c51673abe63e1b4a82828b5ce00d5d771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e41d6e5f7a2a39d36c5abbf9125673c9a8e9a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("shooter_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shooter_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
