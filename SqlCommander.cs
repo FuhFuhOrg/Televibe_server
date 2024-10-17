86e2f9de5c693f0474ab19634ce42302b57a4f7b
SHA-inc-unity <zzz20042013@gmail.com>
2024-10-17 18:10:31 +0300

    add chat create

--- SqlCommander.cs
+++ SqlCommander.cs
@@ -149,157 +149,6 @@ namespace shooter_server
             }
         }
 
-        private int GenerateUniqueUserId(NpgsqlConnection dbConnection)
-        {
-            try
-            {
-                const int max_user_id = int.MaxValue;
-                Random random = new Random();
-
-                int idSender;
-
-                do
-                {
-                    idSender = random.Next(max_user_id);
-
-                    using (var cursor = dbConnection.CreateCommand())
-                    {
-                        cursor.Parameters.AddWithValue("idSender", idSender);
-
-                        cursor.CommandText = $"SELECT COUNT(*) FROM users WHERE id_user = @idSender";
-
-                        long idUserCount = (long)cursor.ExecuteScalar();
-
-                        if (idUserCount > 0)
-                        {
-                            idSender = -1;
-                        }
-                    }
-                } while (idSender == -1);
-
-                return (int)idSender;
-            }
-            catch (Exception e)
-            {
-                //Console.WriteLine($"Error GenerateUniqueUserId command: {e}");
-                return -1;
-            }
-        }
-
-
-        private string GenerateUniqueChatId(NpgsqlConnection dbConnection)
-        {
-            try
-            {
-                Random random = new Random();
-
-                string idChat = "";
-
-                do
-                {
-                    StringBuilder sb = new StringBuilder();
-
-                    sb.Append(random.Next(9) + 1);
-
-                    for (int i = 0; i < 127; ++i)
-                    {
-                        sb.Append(random.Next(10));
-                    }
-
-                    idChat = sb.ToString();
-
-                    using (var cursor = dbConnection.CreateCommand())
-                    {
-                        //Console.WriteLine(idChat + " " + idChat.GetType());
-
-                        cursor.Parameters.AddWithValue("idChat", idChat);
-
-                        cursor.CommandText = $"SELECT COUNT(*) FROM chat WHERE id_chat = @idChat";
-
-                        long idChatCount = (long)cursor.ExecuteScalar();
-
-                        if (idChatCount > 0)
-                        {
-                            idChat = "";
-                        }
-                    }
-                } while (idChat == "");
-
-                return idChat;
-            }
-            catch (Exception e)
-            {
-                //Console.WriteLine($"Error GenerateUniqueUserId command: {e}");
-                return "";
-            }
-        }
-
-
-        private int GenerateUniqueUserIdAccount(NpgsqlConnection dbConnection)
-        {
-            int maxId = -1;
-
-            try
-            {
-                using (var cursor = dbConnection.CreateCommand())
-                {
-                    cursor.CommandText = $"SELECT COALESCE(MAX(id_user), 0) FROM user_account";
-
-                    var result = cursor.ExecuteScalar();
-
-                    if (result != null && result != DBNull.Value)
-                    {
-                        maxId = (int)result;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                //Console.WriteLine($"Error GenerateUniqueUserId command: {e}");
-                return -1;
-            }
-
-            return maxId == -1 ? -1 : maxId + 1;
-        }
-
-
-        // Создание нового чата 
-        private async Task ChatCreate(string sqlCommand, int senderId, NpgsqlConnection dbConnection, Lobby lobby, WebSocket ws)
-        {
-            try
-            {
-                using (var cursor = dbConnection.CreateCommand())
-                {
-                    // ChatCreate requestId isPrivacy chatPassword
-                    List<string> credentials = new List<string>(sqlCommand.Split(' '));
-
-                    credentials.RemoveAt(0);
-                    //Console.WriteLine("\n\n" + "YAAAAAAAAAAAAAAAAAAAAAAA NE JIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIV" + "\n\n");
-
-                    int requestId = int.Parse(credentials[0]);
-                    bool isPrivacy = bool.Parse(credentials[1]);
-                    string chatPassword = credentials.Count == 3 ? credentials[2] : "";
-
-                    string idChat = GenerateUniqueChatId(dbConnection);
-
-                    cursor.CommandText = @"INSERT INTO chat (id_chat, chat_password, is_privacy) VALUES (@idChat, @chatPassword, @isPrivacy);";
-                    cursor.Parameters.AddWithValue("idChat", idChat);
-                    cursor.Parameters.AddWithValue("chatPassword", chatPassword);
-                    cursor.Parameters.AddWithValue("isPrivacy", isPrivacy);
-
-                    await cursor.ExecuteNonQueryAsync();
-
-                    //Console.WriteLine("Chat Created");
-
-                    lobby.SendMessagePlayer(idChat, ws, requestId);
-                }
-            }
-            catch (Exception e)
-            {
-                //Console.WriteLine($"Error ChatCreate command: {e}");
-            }
-        }
-
 
         // Добавить пользователя в чат
         private async Task addUserToChat(string sqlCommand, int senderId, NpgsqlConnection dbConnection, Lobby lobby, WebSocket ws)
@@ -366,245 +215,6 @@ namespace shooter_server
             {
                 //Console.WriteLine($"Error addUserToChat command: {e}");
             }
-        }
-
-        private async Task<(int, string)> CreateGetMessagesStrAsync(NpgsqlConnection dbConnection, List<string> credentials, long kChats, int startIndex)
-        {
-            StringBuilder str = new StringBuilder();
-            int index = startIndex;
-            int returnChatsCount = 0;
-
-            for (int k = 0; k < kChats; k++)
-            {
-                string chatId = credentials[index++];
-                long kSender = long.Parse(credentials[index++]);
-
-                var (adI, missMsg) = await GetMessagesForAuthorsAsync(dbConnection, chatId, credentials, kSender, index);
-                index = adI;
-                if (missMsg == null || missMsg.Count == 0)
-                {
-                    continue;
-                }
-
-                returnChatsCount++;
-                str.Append($" {chatId} {missMsg.Count}");
-
-                foreach (var entry in missMsg)
-                {
-                    var (authorId, publicKey) = entry.Key;
-                    var messages = entry.Value;
-                    int messageCount = messages.Count;
-
-                    if (publicKey == Array.Empty<byte>())
-                    {
-                        str.Append($" {authorId} false {messageCount}");
-                    }
-                    else
-                    {
-                        str.Append($" {authorId} true {Convert.ToBase64String(publicKey)} {messageCount}");
-                    }
-
-                    foreach (var msg in messages)
-                    {
-                        if (msg.is_erase)
-                        {
-                            str.Append($" {msg.id_msg} {msg.is_erase}");
-                        }
-                        else
-                        {
-                            str.Append($" {msg.id_msg} {msg.is_erase} {msg.time_msg.ToString("dd.MM.yyyy HH:mm:ss")} {Convert.ToBase64String(msg.msg)}");
-                        }
-                    }
-                }
-            }
-
-            return (index, returnChatsCount > 0 ? $"{returnChatsCount}{str}" : "");
-        }
-
-
-        private async Task<(int, Dictionary<(int, byte[]), List<Message>>)> GetMessagesForAuthorsAsync(NpgsqlConnection dbConnection, string chatId, List<string> credentials, long kSender, int startIndex)
-        {
-            Dictionary<(int, byte[]), List<Message>> messagesByAuthors = new Dictionary<(int, byte[]), List<Message>>();
-            int index = startIndex;
-
-            List<int> idUsers = new List<int>();
-
-            using (var cursor = dbConnection.CreateCommand())
-            {
-                cursor.Parameters.AddWithValue("chatId", chatId);
-
-                cursor.CommandText = @"SELECT id_user FROM users WHERE id_chat = @chatId";
-
-                using (var reader = await cursor.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        idUsers.Add(reader.GetInt32(0));
-                    }
-                }
-            }
-
-            for (int i = 0; i < kSender; i++)
-            {
-                if (!int.TryParse(credentials[index++], out int authorId))
-                {
-                    //Console.WriteLine($"Invalid authorId at index {index - 1}");
-                    continue;
-                }
-
-                idUsers.Remove(authorId);
-
-                if (!bool.TryParse(credentials[index++], out bool authorKey))
-                {
-                    //Console.WriteLine($"Invalid authorId at index {index - 1}");
-                    continue;
-                }
-
-                if (!int.TryParse(credentials[index++], out int kMsg))
-                {
-                    //Console.WriteLine($"Invalid kMsg at index {index - 1}");
-                    continue;
-                }
-
-                byte[] publicKey = Array.Empty<byte>();
-
-                if (!authorKey)
-                {
-                    using (var cursor = dbConnection.CreateCommand())
-                    {
-                        cursor.Parameters.AddWithValue("authorId", authorId);
-
-                        cursor.CommandText = @"SELECT public_key FROM users WHERE id_user = @authorId";
-
-                        using (var reader = await cursor.ExecuteReaderAsync())
-                        {
-                            while (await reader.ReadAsync())
-                            {
-                                publicKey = reader.GetFieldValue<byte[]>(0);
-                            }
-                        }
-                    }
-                }
-
-                List<int> messageIds = new List<int>();
-                for (int j = 0; j < kMsg; j++)
-                {
-                    if (!int.TryParse(credentials[index++], out int messageId))
-                    {
-                        //Console.WriteLine($"Invalid messageId at index {index - 1}");
-                        continue;
-                    }
-                    messageIds.Add(messageId);
-                }
-
-                using (var command = dbConnection.CreateCommand())
-                {
-                    command.CommandText = @"
-                SELECT m.id_msg, m.time_msg, m.msg, m.is_erase
-                FROM messages m
-                JOIN users u ON m.id_sender = u.id_user
-                WHERE u.id_chat = @chatId 
-                  AND m.id_sender = @authorId
-                  AND ((m.id_msg = ANY(@messageIds) OR m.id_msg > @lastMsgId) OR (m.is_erase = true))
-                ORDER BY m.id_msg";
-                    command.Parameters.AddWithValue("@chatId", chatId);
-                    command.Parameters.AddWithValue("@authorId", authorId);
-                    command.Parameters.AddWithValue("@messageIds", messageIds.ToArray());
-                    command.Parameters.AddWithValue("@lastMsgId", messageIds.Last());
-
-                    using (var reader = await command.ExecuteReaderAsync())
-                    {
-                        while (await reader.ReadAsync())
-                        {
-                            int messageId = reader.GetInt32(0);
-                            DateTime timeMsg = reader.GetDateTime(1);
-                            byte[] msg = reader.GetFieldValue<byte[]>(2);
-                            bool is_erase = reader.GetBoolean(3);
-
-                            if (!messagesByAuthors.ContainsKey((authorId, publicKey)))
-                            {
-                                messagesByAuthors[(authorId, publicKey)] = new List<Message>();
-                            }
-
-                            messagesByAuthors[(authorId, publicKey)].Add(new Message
-                            {
-                                id_msg = messageId,
-                                time_msg = timeMsg,
-                                msg = msg,
-                                is_erase = is_erase
-                            });
-                        }
-                    }
-                }
-            }
-
-            for (int i = 0; i < idUsers.Count; i++)
-            {
-                bool authorKey = false;
-
-                byte[] publicKey = Array.Empty<byte>();
-
-                if (!authorKey)
-                {
-                    using (var cursor = dbConnection.CreateCommand())
-                    {
-                        cursor.Parameters.AddWithValue("authorId", idUsers[i]);
-
-                        cursor.CommandText = @"SELECT public_key FROM users WHERE id_user = @authorId";
-
-                        using (var reader = await cursor.ExecuteReaderAsync())
-                        {
-                            while (await reader.ReadAsync())
-                            {
-                                publicKey = reader.GetFieldValue<byte[]>(0);
-                            }
-                        }
-                    }
-                }
-
-                int messageIds = 0;
-
-                using (var command = dbConnection.CreateCommand())
-                {
-                    command.CommandText = @"
-                SELECT m.id_msg, m.time_msg, m.msg, m.is_erase
-                FROM messages m
-                JOIN users u ON m.id_sender = u.id_user
-                WHERE u.id_chat = @chatId 
-                  AND m.id_sender = @authorId
-                  AND ((m.id_msg >= @messageIds) OR (m.is_erase = true))
-                ORDER BY m.id_msg";
-                    command.Parameters.AddWithValue("@chatId", chatId);
-                    command.Parameters.AddWithValue("@authorId", idUsers[i]);
-                    command.Parameters.AddWithValue("@messageIds", messageIds);
-
-                    using (var reader = await command.ExecuteReaderAsync())
-                    {
-                        while (await reader.ReadAsync())
-                        {
-                            int messageId = reader.GetInt32(0);
-                            DateTime timeMsg = reader.GetDateTime(1);
-                            byte[] msg = reader.GetFieldValue<byte[]>(2);
-                            bool is_erase = reader.GetBoolean(3);
-
-                            if (!messagesByAuthors.ContainsKey((idUsers[i], publicKey)))
-                            {
-                                messagesByAuthors[(idUsers[i], publicKey)] = new List<Message>();
-                            }
-
-                            messagesByAuthors[(idUsers[i], publicKey)].Add(new Message
-                            {
-                                id_msg = messageId,
-                                time_msg = timeMsg,
-                                msg = msg,
-                                is_erase = is_erase
-                            });
-                        }
-                    }
-                }
-            }
-
-            return (index, messagesByAuthors);
         }
 
         private async Task AltSendMessage(string sqlCommand, int senderId, NpgsqlConnection dbConnection, Lobby lobby, WebSocket ws)
@@ -690,6 +300,66 @@ namespace shooter_server
             }
         }
 
+
+
+        public async Task ChatCreate(string sqlCommand, int senderId, NpgsqlConnection dbConnection, WebSocket ws)
+        {
+            try
+            {
+                using (var cursor = dbConnection.CreateCommand())
+                {
+                    // sqlCommand: "ChatCreate requestId isPrivacy chatPassword"
+                    var credentials = sqlCommand.Split(' ').ToList();
+
+                    credentials.RemoveAt(0); // Remove "ChatCreate"
+
+                    int requestId = int.Parse(credentials[0]);
+                    bool isPrivacy = bool.Parse(credentials[1]);
+                    string chatPassword = credentials[2];
+
+                    // Генерация уникального ChatId из 256 байтовых символов
+                    byte[] chatId = GenerateUniqueChatId(dbConnection, preChatId);
+
+                    cursor.CommandText = "INSERT INTO chat (chatid, password, isgroup) VALUES (@chatId, @chatPassword, @isGroup);";
+                    cursor.Parameters.AddWithValue("chatId", chatId);
+                    cursor.Parameters.AddWithValue("chatPassword", Encoding.UTF8.GetBytes(chatPassword));
+                    cursor.Parameters.AddWithValue("isGroup", isPrivacy);
+
+                    await cursor.ExecuteNonQueryAsync();
+
+                    // Отправка сообщения о создании чата клиенту
+                    _lobby.SendMessagePlayer(BitConverter.ToString(chatId).Replace("-", ""), ws, requestId);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in ChatCreate: {e.Message}");
+            }
+        }
+
+        private byte[] GenerateUniqueChatId(NpgsqlConnection dbConnection)
+        {
+            byte[] chatId = new byte[256];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                bool isUnique = false;
+                while (!isUnique)
+                {
+                    rng.GetBytes(chatId);
+
+                    // Проверка на уникальность в базе данных
+                    using (var cursor = dbConnection.CreateCommand())
+                    {
+                        cursor.CommandText = "SELECT COUNT(*) FROM chat WHERE chatid = @chatId;";
+                        cursor.Parameters.AddWithValue("chatId", chatId);
+
+                        var result = cursor.ExecuteScalar();
+                        isUnique = result != null && Convert.ToInt32(result) == 0;
+                    }
+                }
+            }
+            return chatId;
+        }
 
         private async Task Login(string sqlCommand, int senderId, NpgsqlConnection dbConnection, Lobby lobby, WebSocket ws)
         {
