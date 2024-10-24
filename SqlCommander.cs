c186f36237af83fa8d84df816bf557aa1b7d7556
SHA-inc-unity <zzz20042013@gmail.com>
2024-10-24 16:13:52 +0300

    Не приводить числа к инту

--- SqlCommander.cs
+++ SqlCommander.cs
@@ -238,7 +238,7 @@ namespace shooter_server
                     var count = cursor.ExecuteScalar();
 
                     // Если ID уникален, выходим из цикла
-                    if (((int)count) == 0)
+                    if ((count) == 0)
                     {
                         isUnique = true;
                     }
